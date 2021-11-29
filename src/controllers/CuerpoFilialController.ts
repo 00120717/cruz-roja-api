f751de1eb7d7ccc50d909c6d021d7b8c6de0ee95
MickeyMiguel97 <00145016@uca.edu.sv>
2021-11-28 19:44:21 -0600

    controlador cuerpo filial

---
+++ src/controllers/CuerpoFilialController.ts
@@ -0,0 +1,97 @@
+import { CuerpoFilialService } from './../services/CuerpoFilialService';
+import { Request, Response } from 'express';
+import { Container } from 'typedi';
+import { CuerpoFilial } from '../entities/CuerpoFilial';
+import { validate } from 'class-validator';
+
+class CuerpoFilialController {
+  static fetch = async (req: Request, res: Response) => {
+    const cuerpoFilialService = Container.get(CuerpoFilialService);
+    const cuerpoFilialList = await cuerpoFilialService.findAll();
+    res.status(200).send(cuerpoFilialList);
+  }
+
+  static list = async (req: Request, res: Response) => {
+    const cuerpoFilialService = Container.get(CuerpoFilialService);
+    const cuerpoFilialList = await cuerpoFilialService.listAll();
+    res.status(200).send(cuerpoFilialList);
+  }
+
+  static store = async (req: Request, res: Response) => {
+    const cuerpoFilialService = Container.get(CuerpoFilialService);
+    const { name, encargado }: { name: string, encargado : string } = req.body;
+
+    const cuerpoFilial = new CuerpoFilial();
+    cuerpoFilial.nombreCuerpoFilial = name;
+    cuerpoFilial.encargado = encargado;
+
+    const cuerpoFilialErrors = await validate(cuerpoFilial);
+    if (cuerpoFilialErrors.length > 0) {
+      res.status(400).send(cuerpoFilialErrors);
+      return;
+    }
+
+    try {
+      await cuerpoFilialService.create(cuerpoFilial);
+    } catch (e) {
+      res.status(400).json({ message: 'No se pudo crear el cuerpo filial' });
+      return;
+    }
+    res.status(201).json({ message: 'Cuerpo filial creado correctamente' });
+  }
+
+  static update = async (req: Request, res: Response) => {
+    const cuerpoFilialService = Container.get(CuerpoFilialService);
+    const id: number = Number(req.params.id);
+    const { name, encargado }: { name: string, encargado: string } = req.body;
+
+    const cuerpoFilial = await cuerpoFilialService.findById(id);
+    if (!cuerpoFilial) {
+      res.status(404).json({ message: 'Cuerpo filial no encontrado ' });
+      return;
+    }
+
+    cuerpoFilial.nombreCuerpoFilial = name;
+    cuerpoFilial.encargado = encargado;
+
+    const cuerpoFilialErrors = await validate(cuerpoFilial);
+    if (cuerpoFilialErrors.length > 0) {
+      res.status(400).send(cuerpoFilialErrors);
+      return;
+    }
+
+    try {
+      await cuerpoFilialService.update(cuerpoFilial);
+    } catch (e) {
+      res.status(400).json({ message: 'No se pudo actualizar el cuerpo filial' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Cuerpo filial actualizado' });
+  }
+
+  static show = async (req: Request, res: Response) => {
+    const id: number = Number(req.params.id);
+    const cuerpoFilialService = Container.get(CuerpoFilialService);
+    const cuerpoFilial = await cuerpoFilialService.findById(id);
+    if (!cuerpoFilial) {
+      res.status(404).json({ message: 'Cuerpo filial no encontrado ' });
+      return;
+    }
+    res.status(200).send(cuerpoFilial);
+  }
+
+  static destroy = async (req: Request, res: Response) => {
+    const cuerpoFilialService = Container.get(CuerpoFilialService);
+    const id: number = Number(req.params.id);
+    const cuerpoFilial = await cuerpoFilialService.findById(id);
+    if (!cuerpoFilial) {
+      res.status(404).json({ message: 'Cuerpo filial no encontrado' });
+      return;
+    }
+    await cuerpoFilialService.delete(id);
+    res.status(204).send();
+  }
+}
+
+export default CuerpoFilialController;
